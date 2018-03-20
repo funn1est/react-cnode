b97c73a4ce1a96b21f95d0b3084efbca974cb70a
FuNn1esT <admin@funn1est.cn>
2018-03-20 11:39:55 +0800

    build: add webpack-bundle-analyzer

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const merge = require('webpack-merge');
 const base = require('./webpack.config');
 
@@ -46,6 +47,7 @@ const prod = {
       verbose: true,
       dry: false,
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
 
