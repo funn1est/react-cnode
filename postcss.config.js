7e652c52ff435e854b3fc80a52e64782204e0e67
FuNn1esT <admin@funn1est.cn>
2018-03-20 14:48:20 +0800

    build: update postcss-loader configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('autoprefixer')({
+      browsers: [
+        '>1%',
+        'last 4 versions',
+        'not ie < 9',
+      ],
+      flexbox: 'no-2009',
+    }),
   ],
 };
