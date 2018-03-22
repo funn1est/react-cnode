47bdff39620978accaf93707d99d396929c32169
FuNn1esT <admin@funn1est.cn>
2018-03-22 16:06:11 +0800

    feat: add antd zhCN locale support

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 import { asyncRender } from 'utils';
 
 const BasicLayout = asyncRender(
@@ -9,11 +11,13 @@ const BasicLayout = asyncRender(
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={BasicLayout} />
-      </Switch>
-    </Router>
+    <LocaleProvider locale={zhCN}>
+      <Router>
+        <Switch>
+          <Route path="/" component={BasicLayout} />
+        </Switch>
+      </Router>
+    </LocaleProvider>
   );
 };
 
