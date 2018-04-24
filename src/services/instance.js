30a249ad1e84b9e1cc639cba5cc9691478b9536a
FuNn1esT <admin@funn1est.cn>
2018-04-24 12:42:31 +0800

    fix: catch services error

--- src/services/instance.js
+++ src/services/instance.js
@@ -11,9 +11,9 @@ const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
-  const errorText = response.data.msg;
+  const errorText = response.data.error_msg;
   notification.error({
-    message: '错误',
+    message: `请求错误 ${response.status}: ${response.config.url}`,
     description: errorText,
   });
   const error = new Error(errorText);
@@ -23,6 +23,19 @@ const checkStatus = (response) => {
 };
 
 export const get = async (url, params) => {
-  const res = await instance.get(url, { params });
-  return checkStatus(res);
+  try {
+    const res = await instance.get(url, { params });
+    return checkStatus(res);
+  } catch (e) {
+    return checkStatus(e.response);
+  }
+};
+
+export const post = async (url, params) => {
+  try {
+    const res = await instance.post(url, params);
+    return checkStatus(res);
+  } catch (e) {
+    return checkStatus(e.response);
+  }
 };
