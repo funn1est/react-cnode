025e2c030ca080f3cc5c2dd347f9876ff11f26b4
FuNn1esT <admin@funn1est.cn>
2018-05-02 16:01:18 +0800

    feat: add Ellipsis component

---
+++ src/components/Ellipsis/Ellipsis.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './Ellipsis.scss';
+
+const isSupportLineClamp = document.body.style.webkitLineClamp !== undefined;
+
+const Ellipsis = ({ text, className, ...restProps }) => {
+  const cls = classNames(styles.ellipsis, className, {
+    [styles.lines]: !isSupportLineClamp,
+    [styles.lineClamp]: isSupportLineClamp,
+  });
+
+  const id =
+    `ellipsis-${`${new Date().getTime()}${Math.floor(Math.random() * 100)}`}`;
+
+  // support document.body.style.webkitLineClamp
+  if (isSupportLineClamp) {
+    // because postcss flex: no-2009 can't add -webkit-box-orient css property
+    const style = `#${id}{-webkit-line-clamp: 4;-webkit-box-orient: vertical;}`;
+    return (
+      <div id={id} className={cls} {...restProps}>
+        <style>{style}</style>
+        {text}
+      </div>
+    );
+  }
+
+  return (
+    <div className={cls} {...restProps} >
+      {text}
+    </div>
+  );
+};
+
+Ellipsis.propTypes = {
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+
+export default Ellipsis;
