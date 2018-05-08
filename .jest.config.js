e1e7cdbe3e72b94c9ebce85fe1485dfc0cd84f01
FuNn1esT <admin@funn1est.cn>
2018-05-08 18:12:26 +0800

    test: update jest config
    - add snapshot support
    - add mock date support

--- .jest.config.js
+++ .jest.config.js
@@ -1,23 +1,13 @@
 module.exports = {
-  setupFiles: [
-    '<rootDir>/test/setup.js',
-  ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'ts',
-    'tsx',
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-    '/test/',
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}'
-  ],
+  setupFiles: ['<rootDir>/test/setup.js'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/test/'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/utils/registerServiceWorker.js'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/test/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$':
+      '<rootDir>/test/fileMock.js',
     '\\.(css|less|scss|sass)$': '<rootDir>/test/styleMock',
 
     '^common(.*)$': '<rootDir>/src/common$1',
@@ -28,4 +18,5 @@ module.exports = {
     '^store(.*)$': '<rootDir>/src/store$1',
     '^utils(.*)$': '<rootDir>/src/utils$1',
   },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
