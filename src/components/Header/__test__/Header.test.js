e3c7aa1cb07ec4f262392f014bba1b1b3095267c
FuNn1esT <admin@funn1est.cn>
2018-05-12 12:12:48 +0800

    test: add Head re-render test

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import toJson from 'enzyme-to-json';
 import Header from '../Header';
 
 describe('<Header />', () => {
+  const props = {
+    isMobile: false,
+    user: {},
+    onClickMenu: jest.fn(),
+  };
+  const tree = mount(
+    <MemoryRouter>
+      <Header {...props} />
+    </MemoryRouter>,
+  );
+
   it('should render login', () => {
-    const props = {
-      isMobile: false,
-      user: {},
-      onClickMenu: jest.fn(),
-    };
-    const tree = mount(
-      <MemoryRouter>
-        <Header {...props} />
-      </MemoryRouter>,
-    );
+    expect(toJson(tree, { noKey: true, mode: 'deep' })).toMatchSnapshot();
+  });
+
+  it('should re-render when isMobile or user change', () => {
+    tree.setProps({
+      children: cloneElement(tree.props().children, {
+        user: { id: '007', name: 'admin' },
+      }),
+    });
     expect(toJson(tree, { noKey: true, mode: 'deep' })).toMatchSnapshot();
   });
 });
